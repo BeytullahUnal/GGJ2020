d28ad56669c29ed3b38add9ee88a5b0ac5cf75fa
Mustafa Pamir Ünsal <pamir.unsal@gmail.com>
2020-02-01 03:19:13 +0300

    PubSub System & Object Pooler & Example Codes

---
+++ RepairPunk/Assets/Scripts/Pamir/Manager/DummyGameManager.cs
@@ -0,0 +1,21 @@
+﻿using Game_Events;
+using UnityEngine;
+using Utilities.Publisher_Subscriber_System;
+
+namespace Pamir.Manager {
+    public class DummyGameManager : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                PublisherSubscriber.Publish(GameEventType.GameStart);
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                PublisherSubscriber.Publish(GameEventType.GameEnd);
+            }
+        }
+    }
+}
---
+++ RepairPunk/Assets/Scripts/Pamir/Test/PubSubTester.cs
@@ -0,0 +1,41 @@
+﻿using Game_Events;
+using UnityEngine;
+using Utilities.Publisher_Subscriber_System;
+
+namespace Pamir.Test {
+    public class PubSubTester : MonoBehaviour
+    {
+        [SerializeField] private RewindableObjectGroup rewindableObjectGroup;
+        
+        private Subscription<GameEventType> gameStartEventSubscription;
+        private Subscription<GameEventType> gameEndEventSubscription;
+
+        private void OnEnable()
+        {
+            gameStartEventSubscription = PublisherSubscriber.Subscribe<GameEventType>(GameStartEventHandler);
+            gameEndEventSubscription = PublisherSubscriber.Subscribe<GameEventType>(GameEndEventHandler);
+        }
+
+        private void OnDisable()
+        {
+            PublisherSubscriber.Unsubscribe(gameStartEventSubscription);
+            PublisherSubscriber.Unsubscribe(gameEndEventSubscription);
+        }
+
+        private void GameStartEventHandler(GameEventType gameEventType)
+        {
+            if (gameEventType == GameEventType.GameStart)
+            {
+                rewindableObjectGroup.ReleaseObjects();
+            }
+        }
+
+        private void GameEndEventHandler(GameEventType gameEventType)
+        {
+            if (gameEventType == GameEventType.GameEnd)
+            {
+                rewindableObjectGroup.RecallObjects();
+            }
+        }
+    }
+}
